83bdb65910659c5188cdea180dd835e1c21abfa6
Sivakumar <sivakumaar8158@gmail.com>
2026-02-18 19:49:00 +0530

    day36 task

--- NODE/express/mongoose/config/database.js
+++ NODE/express/mongoose/config/database.js
@@ -1,23 +1,23 @@
 import mongoose from "mongoose";
 
 
-const connectmongoose = async ()=>{
+const connectmongoose = async () => {
 
 
 
-    try{
+    try {
 
-        // const conn = await mongoose.connect(process.env.MONGO_URL)
+        const conn = await mongoose.connect(process.env.MONGO_URI)
 
-        const conn = await mongoose.connect(process.env.MONGO_LOCAL_URL)
+        // const conn = await mongoose.connect(process.env.MONGO_LOCAL_URI)
 
-        console.log(`DataBase Connected Successfully ${conn.connection.host}`);
-        
+        console.log(`DataBase Connected Successfully`);
 
-    }catch (error){
 
-        console.log('Something Error',error);
-        
+    } catch (error) {
+
+        console.log('Something Error', error);
+
 
     }
 
--- NODE/express/mongoose/server.js
+++ NODE/express/mongoose/server.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectmongoose from './config/database.js'
+dotenv.config()
 connectmongoose()
 
 const app = express()
 
-dotenv.config()
+
 
 const PORT = process.env.PORT || 5000
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 
 
 app.listen(PORT,()=>{
